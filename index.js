f0119d445991bb91c8832d5f378244503d9459ca
Hunter <EliteSlayer505@gmail.com>
2021-11-30 15:50:35 -0700

    commit tree 88d73d7851f312bf121fc5ade813a0802d4c99ba

--- index.js
+++ index.js
@@ -8,6 +8,22 @@ function distanceFromHqInBlocks(blockNum) {
 function distanceFromHqInFeet(blockNum) {
     return distanceFromHqInBlocks(blockNum) * 264
 }
-function distanceTravelledInFeet(blockNum2, blocknum3){
-    
+function distanceTravelledInFeet(start, end){
+    if (start < end){
+        return (end - start) * 264
+    }else {
+        return (start - end) * 264
+    }
+}
+function calculatesFarePrice(start, end) {
+    const distance = distanceTravelledInFeet(start, end)
+    if (distance < 400) {
+        return (end - start) - 1
+    }else if (distance >= 400 && distance <= 2000) {
+        return (128 * .02)
+    }else if (distance > 2000 && distance < 2500) {
+        return (25)
+    }else {
+        return 'cannot travel that far'
+    }
 }
