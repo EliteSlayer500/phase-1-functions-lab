483fa69cee14e0d630ffa2322225c06bca6c00dd
Hunter <EliteSlayer505@gmail.com>
2021-11-30 14:53:44 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blockNum) {
+    if (blockNum < 42) {
+      return 42 - blockNum
+    } else {
+      return  blockNum - 42
+    }
+}
+function distanceFromHqInFeet(blockNum) {
+    return distanceFromHqInBlocks(blockNum) * 264
+}
+function distanceTravelledInFeet(blockNum2, blocknum3){
+    
+}
